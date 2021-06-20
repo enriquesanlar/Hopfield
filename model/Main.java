f85526c1b856743fbb796426a217a13ef843ec87
Enrique <enriquesanchezlar@ciencias.unam.mx>
2021-06-19 19:09:52 -0500

    first commit

---
+++ model/Main.java
@@ -0,0 +1,12 @@
+package model;
+
+import processing.core.PApplet;
+import processing.core.PFont;
+
+public class Main extends PApplet {
+    
+    public static void main(String args[]) {
+        PApplet.main(new String[]{"model.Main"});
+    }
+
+}
